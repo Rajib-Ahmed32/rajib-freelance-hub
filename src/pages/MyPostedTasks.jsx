9237f66487ffdde361730804c8736ee264459af5
Rajib Ahmed <rajibahmed5434@gmail.com>
2025-05-20 21:37:21 +0600

    feat: initialize project with folder structure, install essential packages, and implement main routing setup with dynamic routes

---
+++ src/pages/MyPostedTasks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyPostedTasks = () => {
+  return <div>MyPostedTasks</div>;
+};
+
+export default MyPostedTasks;
---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home";
+import AddTask from "../pages/AddTask";
+import Login from "../pages/Login";
+import MyPostedTasks from "../pages/MyPostedTasks";
+import TaskDetails from "../pages/TaskDetails";
+import UpdateTask from "../pages/UpdateTask";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/add-task", element: <AddTask /> },
+      { path: "/posted-task", element: <MyPostedTasks /> },
+      { path: "/task-details/:taskId", element: <TaskDetails /> },
+      { path: "/update-task/:taskId", element: <UpdateTask /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
+
+export default router;
